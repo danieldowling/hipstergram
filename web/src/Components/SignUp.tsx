71125fea20e7c8d6d3403a1a7398570ceed56959
Daniel Dowling <danieldowling@Daniels-MBP.home>
2021-12-10 16:00:46 -0500

    minor styling to signup, added tailwind css

--- web/src/Components/SignUp.tsx
+++ web/src/Components/SignUp.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import '../tailwind.output.css';
 
 const SignUp: React.FC = () => {
   const [username, setUsername] = useState('');
@@ -26,14 +27,32 @@ const SignUp: React.FC = () => {
 
   };
 
-  return <div>
-    <h3>Sign Up Here!</h3>
-
-  <label>Username:</label>
-  <input value={username} onChange={(e) => setUsername(e.target.value)}/>
-  <label>Password:</label>
-  <input value={password} onChange={(e) => setPassword(e.target.value)}/>
-  <button onClick={handleClick}>Click Me Please</button>
+  return <div className="container mx-auto">
+      <div className="flex justify-center">
+        <form className="my-8">
+          <div>
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">Username:</label>
+            </div>
+            <div>
+              <input value={username} onChange={(e) => setUsername(e.target.value)} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Enter Email"/>
+            </div>
+          </div>
+          <div>
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-password">Password:</label>
+            </div>
+            <div>
+              <input value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************"/>
+            </div>
+          </div>
+          <div className="md:flex md:items-center">
+              <div className="mx-auto p-5">
+                <button onClick={handleClick} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">Click Me To Sign Up</button>
+              </div>
+          </div>
+        </form>
+      </div>
   </div>
 };
 
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,7 +4,11 @@ import SignUp from './Components/SignUp';
 const App = () => {
   return (
     <div>
-      <h1>Hipstergram.</h1>
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <span className="font-semibold text-xl tracking-tight">Hipstergram.</span>
+        </div>
+      </nav>
       <SignUp />
     </div>
   );
